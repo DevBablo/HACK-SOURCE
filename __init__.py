833c72c439ab05c860367c2554176b59d00c46fd
DevBablo <ssayf4831@gmail.com>
2024-05-20 23:09:43 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from .func import *
+from env import MONGO_URL
+from logger import LOGGER
+from Hack.database.mongo import Mongo
+
+DB = None
+if MONGO_URL:
+    LOGGER(__name__).info('Database Intialised')
+    DB = Mongo(MONGO_URL)
